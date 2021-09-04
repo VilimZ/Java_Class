b7349d1d3455886c91ade9dc088ab31219c3339f
Vilim <vilim.zovko@gmail.com>
2021-09-04 15:42:51 +0200

    Add second instances.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
         System.out.println("Kathryn  ");
 
         Car myCar = new Car(25.4, "MO-223-P", Color.BLUE, true);
+        Car sallyCar = new Car(13.5, "ZG-77-LE",Color.WHITE,true);
 
         System.out.println("My Car license plate: " + myCar.licensePlate);
+        System.out.println("Sally's car license plate is: " +sallyCar.licensePlate);
 
     }
 
